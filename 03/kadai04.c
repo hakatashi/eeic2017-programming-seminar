dc8061c442724f2a5e0c517490c9ed2e7658c686
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-12 11:23:38 +0900

    Add 03/kadai04.c

---
+++ 03/kadai04.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void factorize(int, int);
+
+int main() {
+	int n = 123456789;
+	printf("Prime factor of %d",n);
+	printf("%d = ", n);
+	factorize(n, 2);
+}
+
+void factorize(int n, int factor) {
+	int i;
+
+	for (i = factor; i * i <= n; i++) {
+		if (n % i == 0) {
+			printf("%d * ", i);
+			factorize(n / i, i);
+			return;
+		}
+	}
+
+	printf("%d\n", n);
+	return;
+}
