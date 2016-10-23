24998ef77ea79d146ee9ff6999796187f81da1e8
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-24 01:06:06 +0900

    Implement tail and match test runner

--- test.sh
+++ test.sh
@@ -13,6 +13,8 @@ testKadaiScripts() {
     infile="test/$i.in.txt"
     outfile="test/$i.out.txt"
     headfile="test/$i.head.txt"
+    tailfile="test/$i.tail.txt"
+    matchfile="test/$i.match.txt"
 
     if test -e "$infile"; then
       result=`./temp < $infile`
@@ -27,10 +29,20 @@ testKadaiScripts() {
 
     if test -e "$headfile"; then
       expected=`cat "$headfile"`
-      assertTrue "<${result:0:1000}...> did not head with <$expected>" "[[ '$result' == '$expected'* ]]"
+      assertTrue "<${result:0:1000}...> doesn't head with <$expected>" "[[ '$result' == '$expected'* ]]"
     fi
 
-    if test ! -e "$outfile" && test ! -e "$headfile"; then
+    if test -e "$tailfile"; then
+      expected=`cat "$tailfile"`
+      assertTrue "<${result:0:1000}...> doesn't tail with <$expected>" "[[ '$result' == *'$expected' ]]"
+    fi
+
+    if test -e "$matchfile"; then
+      expected=`cat "$matchfile"`
+      assertTrue "<${result:0:1000}...> doesn't include <$expected>" "[[ '$result' == *'$expected'* ]]"
+    fi
+
+    if test ! -e "$outfile" && test ! -e "$headfile" && test ! -e "$tailfile" && test ! -e "$matchfile"; then
       echo "WARNING: $i has no spec files"
     fi
 
