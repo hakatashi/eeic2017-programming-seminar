76c9360c41357832efe21901364e8d7060065dc5
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-24 01:33:46 +0900

    Fix 02/kadai08

--- 02/kadai08.c
+++ 02/kadai08.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#define LOOP(x, n) for(x = 1; x < n; x++)
-#define ILOOP(x, n) for(x = 1; x <= n; x++)
+#define LOOP(x, f, t) for(x = f; x < t; x++)
+#define ILOOP(x, f, t) for(x = f; x <= t; x++)
 
 int gcd(int, int);
 
 int main() {
 	int x, y, z;
 
-	LOOP(y, 1000) ILOOP(x, y) LOOP(z, 1000)
+	LOOP(x, 1, 1000) ILOOP(y, x, 1000) LOOP(z, 1, 1000)
 		if (x * x + y * y == z * z && gcd(x, y) == 1)
 			printf("%d^2 + %d^2 = %d^2\n", x, y, z);
 
