4ada88aaa0b41900237103c91cb8eb51b90ed85d
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-24 01:52:26 +0900

    Eliminate warnings in 02/kadai02.c

--- 02/kadai02.c
+++ 02/kadai02.c
@@ -8,7 +8,7 @@ int main() {
 	size_t size;
 
 	while (getline(&line, &size, stdin) != -1) {
-		for (i = 0; i < strlen(line) && !isspace(line[i]); i++) {
+		for (i = 0; i < strlen(line) && !isspace((unsigned char)line[i]); i++) {
 			putchar(line[i]);
 		}
 		putchar('\n');
